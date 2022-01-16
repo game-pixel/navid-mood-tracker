6fead957b4779f44a8ef60371e9cf82666b1e020
game-pixel <divancoder10@gmail.com>
2022-01-16 03:46:24 +0000

    update

--- src/lib/EntryModal.svelte
+++ src/lib/EntryModal.svelte
@@ -58,3 +58,118 @@
    						 <label for="dayInput">Day</label>
    					 </form>
    				 </div>
+                    <div class="col">
+   					 <form class="form-floating">
+   						 <input
+   							 type="number"
+   							 class="form-control"
+   							 id="monthInput"
+   							 bind:value={month}
+   							 min="1"
+   							 max="12"
+   						 />
+   						 <label for="monthInput">Month</label>
+   					 </form>
+   				 </div>
+   				 <div class="col">
+   					 <form class="form-floating">
+   						 <input
+   							 type="number"
+   							 class="form-control"
+   							 id="yearInput"
+   							 bind:value={year}
+   							 min="2021"
+   						 />
+   						 <label for="yearInput">Year</label>
+   					 </form>
+   				 </div>
+   			 </div>
+
+   			 <!-- Mood Scale -->
+   			 <div class="row mt-3 text-center">
+   				 <h5>How was your day?</h5>
+   				 <h1 class="display-1">{emoji}</h1>
+   			 </div>
+   			 <div class="row px-5">
+   				 <div class="btn-group mt-2 mb-3" role="group">
+   					 <input
+   						 type="radio"
+   						 class="btn-check"
+   						 name="moodRadio"
+   						 id="worst"
+   						 autocomplete="off"
+   						 on:click={() => {
+   							 emoji = emojiList.worst;
+   							 mood = 'Worst';
+   						 }}
+   					 />
+   					 <label class="btn btn-outline-danger" for="worst">Worst</label>
+
+   					 <input
+   						 type="radio"
+   						 class="btn-check"
+   						 name="moodRadio"
+   						 id="bad"
+   						 autocomplete="off"
+   						 on:click={() => {
+   							 emoji = emojiList.bad;
+   							 mood = 'Bad';
+   						 }}
+   					 />
+   					 <label class="btn btn-outline-warning" for="bad">Bad</label>
+
+   					 <input
+   						 type="radio"
+   						 class="btn-check"
+   						 name="moodRadio"
+   						 id="okay"
+   						 autocomplete="off"
+   						 checked
+   						 on:click={() => {
+   							 emoji = emojiList.okay;
+   							 mood = 'Okay';
+   						 }}
+   					 />
+   					 <label class="btn btn-outline-primary" for="okay">Okay</label>
+
+   					 <input
+   						 type="radio"
+   						 class="btn-check"
+   						 name="moodRadio"
+   						 id="good"
+   						 autocomplete="off"
+   						 on:click={() => {
+   							 emoji = emojiList.good;
+   							 mood = 'Good';
+   						 }}
+   					 />
+   					 <label class="btn btn-outline-info" for="good">Good</label>
+
+   					 <input
+   						 type="radio"
+   						 class="btn-check"
+   						 name="moodRadio"
+   						 id="best"
+   						 autocomplete="off"
+   						 on:click={() => {
+   							 emoji = emojiList.best;
+   							 mood = 'Best';
+   						 }}
+   					 />
+   					 <label class="btn btn-outline-success" for="best">Best</label>
+   				 </div>
+   			 </div>
+
+   			 <!-- Comments -->
+   			 <div class="form-floating">
+   				 <textarea class="form-control" bind:value={comment} id="comment" />
+   				 <label for="commentTextarea">Comments</label>
+   			 </div>
+   		 </div>
+   		 <div class="modal-footer">
+   			 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+   			 <button type="button" class="btn btn-primary" on:click={saveEntry}>Add</button>
+   		 </div>
+   	 </div>
+    </div>
+</div>
